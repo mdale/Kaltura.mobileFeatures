32770ca544995ebf3d7119883d216ddf1d1b3a41
michael dale <dale@michaels-MacBook-Air.local>
2012-01-29 04:17:51 +0200

    Initial Commit

---
+++ mobileFeatures/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  mobileFeatures
+//
+//  Created by michael dale on 1/29/12.
+//  Copyright Kaltura 2012. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppDelegate");
+    [pool release];
+    return retVal;
+}
